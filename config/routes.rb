7e4a3a391475c519719bd66ec9a2abf0a5df50e6
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-09-03 01:46:34 +0900

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   	get 'home/about'
 
   	resources :users, only: [:show, :index, :edit, :update]
-  	resources :books do
-  		resource :favorites, only: [:create, :destroy]
+    get 'users/:id/follower' => 'users#follower', as: "follower"
+    get 'users/:id/followed' => 'users#followed', as: "followed"
+  	post 'follow/:id' => 'relationships#follow', as: "follow"
+  	post 'unfollow/:id' => 'relationships#unfollow', as: "unfollow"
+
+  	resources :books, except: [:new] do
   		resources :book_comments, only: [:create, :destroy]
+  		resource :favorites, only: [:create, :destroy]
   	end
-
-
 end
