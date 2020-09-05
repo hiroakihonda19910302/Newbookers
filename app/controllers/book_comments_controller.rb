be79310ff81f4c63a409081025bd3dda28794355
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-09-05 20:07:21 +0900

    [Finish] 応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,10 +4,8 @@ class BookCommentsController < ApplicationController
 		@book = Book.find(params[:book_id])
     	@book_comment = current_user.book_comments.new(book_comment_params)
     	@book_comment.book_id = @book.id
-    	if @book_comment.save
-      	flash[:success] = "Comment was successfully created."
-    	end
-    	redirect_to request.referer
+    	@book_comment.save
+      flash[:success] = "Comment was successfully created."
   end
   	
 
@@ -15,7 +13,6 @@ class BookCommentsController < ApplicationController
 		@book = Book.find(params[:book_id])
     	book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
     	book_comment.destroy
-    	redirect_to request.referer
 	end
 
 	private
