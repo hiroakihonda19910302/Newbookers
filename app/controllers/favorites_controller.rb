49a59a2fe16550eecaed7aa5db88269197e5fbb9
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-09-06 19:45:01 +0900

    [Finish] 応用課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
 
 	def create
 		@book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
   	end
 
@@ -11,4 +11,14 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
   	end
+
+    private
+    def redirect
+      case params[:redirect_id].to_i
+      when 0
+        redirect_to books_path
+      when 1
+        redirect_to book_path(@book)
+      end
+    end
 end
